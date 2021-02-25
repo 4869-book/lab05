f11326e480817296d0725542cef4af310c315a21
Phongsathon Wandee <phongsathon.book@mail.kmutt.ac.th>
2021-02-25 14:44:10 +0700

    final commit

--- 02/todo.js
+++ 02/todo.js
@@ -1,22 +1,32 @@
 function addlist() {
     var text = document.getElementById("myText");
     var node = document.createElement("li");
-    var textnode = document.createTextNode(text.value);
-    node.appendChild(textnode);
-    document.getElementById("myList").appendChild(node);
-    text.value="";
+    var textnode = document.createTextNode(" "+text.value);
+    var image = document.createElement('img');
+    image.src="https://cdn.pixabay.com/photo/2012/04/12/20/12/x-30465_640.png";
+    if(text.value!==""){
+      node.appendChild(image);
+      node.appendChild(textnode);
+      document.getElementById("myList").appendChild(node);
+      text.value="";
+      node.addEventListener('click',addDone);
+      image.addEventListener('click',delList);
+    }
+    
   }
-
+  
 var input = document.getElementById("myText");
 input.addEventListener("keypress", function(event) {
 if (event.keyCode === 13) {
     addlist();
-    var list = document.querySelector('li');
-    list.classList.add("fa fa-trash");
   }
 });
 
-function listcross(){
-    console.log(list.length);
+function addDone(evt){
+    evt.currentTarget.classList.toggle('done');
 }
-var list = document.querySelectorAll('li');
+
+function delList(evt){
+  evt.currentTarget.parentElement.remove();
+}
+
